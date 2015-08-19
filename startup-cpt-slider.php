553414de6c244bc51be6da0ab81acee9eb1b65e1
yozz <yozz@Manava.local>
2015-08-18 21:54:00 -0400

    GitHub Plugin Updater

--- startup-cpt-slider.php
+++ startup-cpt-slider.php
@@ -1,10 +1,34 @@
 <?php
 /*
-Plugin Name: StartUp Slider Custom Post Type
+Plugin Name: StartUp Slider
 Description: Le plugin pour activer le Custom Post Slider
 Author: Yann Caplain
-Version: 1.2.0
+Version: 1.3.0
 */
+
+//GitHub Plugin Updater
+function startup_reloaded_slider_updater() {
+	include_once 'lib/updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) {
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'startup-cpt-slider',
+			'api_url' => 'https://api.github.com/repos/yozzi/startup-cpt-slider',
+			'raw_url' => 'https://raw.github.com/yozzi/startup-cpt-slider/master',
+			'github_url' => 'https://github.com/yozzi/startup-cpt-slider',
+			'zip_url' => 'https://github.com/yozzi/startup-cpt-slider/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
+
+add_action( 'init', 'startup_reloaded_slider_updater' );
 
 //CPT
 function startup_reloaded_slider() {
